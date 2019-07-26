5922e37d88d0d90976ccebbe56200573439a61e8
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-26 18:44:18 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,20 @@
+class Course
 
+  @@all = []
+  attr_accessor :title, :schedule, :description
 
+  def initialize
+    @title = title
+    @schedule = schedule
+    @description = description
+    @@all << self
+  end
+
+ def self.all
+   @@all
+ end
+
+ def self.reset_all
+   @@all.clear
+ end
+end
